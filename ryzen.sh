b77eca4addceb754918e484b8e57ff968726123c
RooGhz720 <rooghz720@gmail.com>
2023-01-28 12:52:25 +0800

    Update
    Signed-off-by: fchelz <fchelzz@gmail.com>

--- ryzen.sh
+++ ryzen.sh
@@ -10,6 +10,47 @@
 #
 # Use this script on root of kernel directory
 
+# setup telegram env
+API_BOT="6787166379:AAGXuTzT49V0DdAzLiRB4Lj3PUsVQWkIiJM"
+CHATID="-4064889762"
+export CHATID API_BOT TYPE_KERNEL
+export WAKTU=$(date +"%T")
+export TGL=$(date +"%d-%m-%Y")
+export BOT_MSG_URL="https://api.telegram.org/bot$API_BOT/sendMessage"
+export BOT_BUILD_URL="https://api.telegram.org/bot$API_BOT/sendDocument"
+
+tg_sticker() {
+   curl -s -X POST "https://api.telegram.org/bot$API_BOT/sendSticker" \
+        -d sticker="$1" \
+        -d chat_id=$CHATID
+}
+
+tg_post_msg() {
+        curl -s -X POST "$BOT_MSG_URL" -d chat_id="$2" \
+        -d "parse_mode=markdown" \
+        -d text="$1"
+}
+
+tg_post_build() {
+        #Post MD5Checksum alongwith for easeness
+        MD5CHECK=$(md5sum "$1" | cut -d' ' -f1)
+
+        #Show the Checksum alongwith caption
+        curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
+        -F chat_id="$2" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=markdown" \
+        -F caption="$3 MD5 \`$MD5CHECK\`"
+}
+
+tg_error() {
+        curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
+        -F chat_id="$2" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="$3Failed to build , check <code>error.log</code>"
+}
+
 bold=$(tput bold)
 normal=$(tput sgr0)
 
@@ -25,12 +66,20 @@ done
 [[ -z ${ZIP} ]] && { echo "${bold}Gunakan -Z atau --zip Untuk Membuat Zip Kernel Installer${normal}"; }
 
 # Clone toolchain
-if ! [ -d "../toolchain" ]; then
-    wget -O proton.tar.zst https://github.com/kdrag0n/proton-clang/archive/20200801.tar.gz
-    mkdir -p ../toolchain/clang12.0
-    sudo tar -I zstd -xvf proton.tar.zst -C ../toolchain/clang --strip-components=1
-else
-    echo "${bold}Folder Toolchain Sudah Tersedia, Tidak Perlu Di Clone${normal}"
+#if ! [ -d "../toolchain" ]; then
+#    wget -O proton.tar.zst https://github.com/kdrag0n/proton-clang/archive/20200801.tar.gz
+#    mkdir -p ../toolchain/clang12.0
+#    sudo tar -I zstd -xvf proton.tar.zst -C ../toolchain/clang --strip-components=1
+#else
+#    echo "${bold}Folder Toolchain Sudah Tersedia, Tidak Perlu Di Clone${normal}"
+#fi
+
+if ! [ -d "$HOME/cosmic" ]; then
+echo "Cosmic clang not found! Cloning..."
+if ! git clone -q https://gitlab.com/GhostMaster69-dev/cosmic-clang.git --depth=1 -b master ~/cosmic; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
 fi
 
 # ENV
@@ -38,15 +87,13 @@ CONFIG=vendor/ginkgo-perf_defconfig
 KERNEL_DIR=$(pwd)
 PARENT_DIR="$(dirname "$KERNEL_DIR")"
 KERN_IMG="$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb"
-export KBUILD_BUILD_USER="EdwiinKJ"
-export KBUILD_BUILD_HOST="AMDRyzen"
-export PATH="$PARENT_DIR/toolchain/nusantaraclang12/bin:$PATH"
-export LD_LIBRARY_PATH="$PARENT_DIR/toolchain/nusantaraclang12/lib:$LD_LIBRARY_PATH"
-export KBUILD_COMPILER_STRING="$("$PARENT_DIR"/toolchain/nusantaraclang12/bin/clang --version | head -n 1 | perl -pe 's/\((?:http|git).*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//' -e 's/^.*clang/clang/')"
-
+export KBUILD_BUILD_USER="root"
+export KBUILD_BUILD_HOST="xderm"
+export PATH="$HOME/cosmic/bin:$PATH"
+export KBUILD_COMPILER_STRING="$($HOME/cosmic/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 # Functions
 clang_build () {
-    make -j4 O=out \
+    make -j O=out \
                           ARCH=arm64 \
                           CC="clang" \
                           AR="llvm-ar" \
@@ -85,3 +132,30 @@ make -C "$ZIP_DIR" clean
 wifi_modules
 cp "$KERN_IMG" "$ZIP_DIR"/
 make -C "$ZIP_DIR" normal
+
+# stiker post
+echo "Uploading your kernel.."
+DEVICE="Redmi Note 8"
+MESIN="Git Workflows"
+DATE=$(date +"%Y%m%d-%H%M%S")
+KOMIT=$(git log --pretty=format:'"%h : %s"' -2)
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+TYPE="MIUI"
+
+TEXT1="
+*Build Completed Successfully*
+━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━
+* Device* : \`$DEVICE\`
+* Code name* : \`Ginkgo\`
+* Variant Build* : \`$TYPE\`
+* Branch Build* : \`$BRANCH\`
+* System Build* : \`$MESIN\`
+* Date Build* : \`$TGL\` \`$WAKTU\`
+* Last Commit* : \`$KOMIT\`
+* Author* : @fchelz
+━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━"
+
+                tg_post_msg "$TEXT1" "$CHATID"
+                tg_post_build "$ZIP_DIR" "$CHATID"
+exit
+
